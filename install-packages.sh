00decc841fbaf9e07e2e30e1d8d7d4a8228dcced
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-04-25 19:26:32 -0500

    feat: Tweaked neomutt.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- install-packages.sh
+++ install-packages.sh
@@ -7,7 +7,7 @@ if [ "$DISTRO" = "VoidLinux" ]; then
   sudo xbps-install -Sy polkit chrony dbus \
       pulseaudio NetworkManager network-manager-applet \
       base-devel curl wget git git-lfs jq unzip zstd zsh \
-      vim neovim neofetch cmake go zig \
+      vim neovim neofetch cmake go zig cronie tzupdate \
       Thunar thunar-volman gvfs tumbler ffmpegthumbnailer \
       lm_sensors pulsemixer brightnessctl \
       maim xclip feh fd picom ripgrep \
@@ -27,9 +27,10 @@ if [ "$DISTRO" = "VoidLinux" ]; then
 
   echo "[install-packages] Please check that the following services are enables in /var/service"
   echo "[install-packages]    polkitd"
-  echo "[install-packages]    chronyd"
+  echo "[install-packages]    ntpd"
   echo "[install-packages]    dbus"
   echo "[install-packages]    NetworkManager"
+  echo "[install-packages]    crond"
 else
   echo "[install-packages] Not yet implemented for $DISTRO. Nothing to install. Check manually if something does not work"
 fi
