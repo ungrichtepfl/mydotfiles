442ebf6d6b6bfd7fa172156391e4a5497b5e1ce2
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-07-18 22:51:45 +0800

    fix: Remove kitty terminal.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- install-packages.sh
+++ install-packages.sh
@@ -17,7 +17,7 @@ if [ "$DISTRO" = "VoidLinux" ]; then
         dua-cli yazi poppler wiki-tui delta \
         i3 i3status-rust dmenu i3lock-color \
         lightdm lightdm-gtk3-greeter elogind \
-        flatpak gthumb mupdf kitty \
+        flatpak gthumb mupdf \
         neomutt khard w3m vlc inkscape \
         nerd-fonts font-awesome6 flameshot
     if [ "$1" = "--work" ] || [ "$1" = "-w" ]; then
