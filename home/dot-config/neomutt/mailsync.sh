dd2dbaca48a76d2cc059ca1d86dc0cf55c567a1c
Christoph Ungricht <christoph.ungricht@outlook.com>
2025-08-16 11:05:50 +0800

    fix: Piping to /dev/null.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- home/dot-config/neomutt/mailsync.sh
+++ home/dot-config/neomutt/mailsync.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 function main {
-    if pgrep goimapnotify 2&> /dev/null; then
+    if pgrep goimapnotify &> /dev/null; then
         echo "gomapnotify already running."
         exit 0
     fi
 
     while true; do
-        while ping -c 1 "8.8.8.8" 2&> /dev/null; do
+        while ping -c 1 "8.8.8.8" &> /dev/null; do
             sleep 1
         done
-        if ! pgrep goimapnotify 2&> /dev/null; then
+        if ! pgrep goimapnotify &> /dev/null; then
             goimapnotify
         fi
         sleep 5
