f6490db922da4bf4590aac2db888dab85e6749da
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-01-08 15:54:15 +0100

    feat: Add directory as argument.
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- home/dot-config/neomutt/new_email_notify.sh
+++ home/dot-config/neomutt/new_email_notify.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-maildirnew="$HOME/Mail/*/INBOX/new/"
+maildirnew="$HOME/Mail/$1/INBOX/new/"
 new="$(find $maildirnew -type f | wc -l)"
 
 if [ "$new" -gt 0 ]; then
-    notify-send -a "Neomutt" "$1" "$2"
+    notify-send -a "Neomutt" "$2" "$3"
 fi
