8535e4dd0d38d3cf54228042243f576784d0705d
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-02-08 16:40:28 +0100

    fix: delete key in zsh
    Signed-off-by: Christoph Ungricht <christoph.ungricht@outlook.com>

--- zsh/dot-zsh.d/keybinds.zsh
+++ zsh/dot-zsh.d/keybinds.zsh
@@ -1,4 +1,7 @@
 bindkey -e # Select emacs keybindings to be compatible with bash
 autoload edit-command-line
 zle -N edit-command-line
-bindkey '^X^E' edit-command-line
+bindkey  "^X^E"   edit-command-line
+bindkey  "^[[H"   beginning-of-line # HOME
+bindkey  "^[[F"   end-of-line       # END
+bindkey  "^[[3~"  delete-char       # DELETE
