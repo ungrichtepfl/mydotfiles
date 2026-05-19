993cde766755e7e71be1e1a5eeaedf23c12c2141
Christoph Ungricht <christoph.ungricht@outlook.com>
2026-05-19 07:54:58 +0200

    feat: add nice zsh keybinds

--- zsh/dot-zsh.d/keybinds.zsh
+++ zsh/dot-zsh.d/keybinds.zsh
@@ -5,3 +5,18 @@ bindkey  "^X^E"   edit-command-line
 bindkey  "^[[H"   beginning-of-line # HOME
 bindkey  "^[[F"   end-of-line       # END
 bindkey  "^[[3~"  delete-char       # DELETE
+bindkey  "^[z"    undo              # Nicer for swiss keyboard than C-_
+bindkey  "^[Z"    redo
+
+bindkey -s '^f' 'cd "$(fd -t d . | fzf)"\n'
+
+yazicd () {
+    tmp="$(mktemp)"
+    yazi --cwd-file="$tmp" "$@"
+    if [ -f "$tmp" ]; then
+        dir="$(cat "$tmp")"
+        rm -f "$tmp" >/dev/null
+        [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+    fi
+}
+bindkey -s '^o' 'yazicd\n'
